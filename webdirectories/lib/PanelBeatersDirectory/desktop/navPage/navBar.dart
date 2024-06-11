ed327899a6b337e557529921dd4d6181322f91df
JacoHg <168348167+JacoHg@users.noreply.github.com>
2024-06-11 15:31:19 +0200

    nav bar changes

--- webdirectories/lib/PanelBeatersDirectory/desktop/navPage/navBar.dart
+++ webdirectories/lib/PanelBeatersDirectory/desktop/navPage/navBar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:ui'; // Import for ImageFilter
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:webdirectories/myutility.dart';
 
 class NavBar extends StatefulWidget {
   final Function(int) onIconTapped;
@@ -18,6 +19,16 @@ class NavBar extends StatefulWidget {
     'images/ownersportal.svg',
   ];
 
+  final List<String> menuTooltips = [
+    'PBD Home',
+    'Featured',
+    'Job Finder',
+    '#WeConnect',
+    'Articles',
+    'Shop',
+    'Owners Login'
+  ];
+
   NavBar({required this.onIconTapped});
 
   @override
@@ -31,18 +42,19 @@ class _NavBarState extends State<NavBar> {
   Widget build(BuildContext context) {
     return Container(
       width: 60,
-      height: 1167,
+      height: MyUtility(context).height,
       decoration: ShapeDecoration(
         gradient: LinearGradient(
-          begin: Alignment(0.56, -0.83),
-          end: Alignment(-0.56, 0.83),
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
           colors: [
-            Colors.white.withOpacity(0.10),
-            Colors.white.withOpacity(0.40),
+            Color.fromARGB(108, 255, 255, 255),
+            const Color.fromARGB(59, 255, 255, 255),
           ],
         ),
         shape: RoundedRectangleBorder(
           side: BorderSide(
+              
               width: 1,
               strokeAlign: BorderSide.strokeAlignOutside,
               color: Colors.white),
@@ -60,45 +72,83 @@ class _NavBarState extends State<NavBar> {
         children: [
           Column(
             children: List.generate(
-              6,
+              5,
               (index) => Padding(
                 padding: const EdgeInsets.symmetric(vertical: 12.0),
-                child: IconButton(
-                  icon: SvgPicture.asset(
-                    widget.iconPaths[index],
-                    width: 40,
-                    height: 40,
-                    color: selectedIndex == index
-                        ? Color(0xFFFF8828)
-                        : Colors.white,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      selectedIndex = index;
-                    });
+                child: Tooltip(
+                  message: widget.menuTooltips[index],
+                  textStyle: TextStyle(
+                      color: Color(0xFFEF9040), fontFamily: 'raleway'),
+                  child: IconButton(
+                    icon: SvgPicture.asset(
+                      widget.iconPaths[index],
+                      width: 40,
+                      height: 40,
+                      color: selectedIndex == index
+                          ? Color(0xFFFF8828)
+                          : Colors.white,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        selectedIndex = index;
+                      });
 
-                    widget.onIconTapped(index);
-                  },
+                      widget.onIconTapped(index);
+                    },
+                  ),
                 ),
               ),
             ),
           ),
+          Spacer(),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 0.0),
-            child: IconButton(
-              icon: SvgPicture.asset(
-                widget.iconPaths[6],
-                width: 50,
-                height: 50,
-                color: selectedIndex == 6 ? Color(0xFFFF8828) : Colors.white,
-              ),
-              onPressed: () {
-                setState(() {
-                  selectedIndex = 6;
-                });
+            child: Tooltip(
+              message: widget.menuTooltips[5],
+              
+                  textStyle: TextStyle(
+                      color: Color(0xFFEF9040), fontFamily: 'raleway'),
 
-                widget.onIconTapped(6);
-              },
+              child: IconButton(
+                
+                icon: SvgPicture.asset(
+                  widget.iconPaths[5],
+                  width: 50,
+                  height: 50,
+                  color: selectedIndex == 5 ? Color(0xFFFF8828) : Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 5;
+                  });
+              
+                  widget.onIconTapped(5);
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Tooltip(
+              message: widget.menuTooltips[6],
+                  textStyle: TextStyle(
+                      color: Color(0xFFEF9040), fontFamily: 'raleway'),
+              child: IconButton(
+                
+                icon: SvgPicture.asset(
+                  widget.iconPaths[6],
+                  width: 50,
+                  height: 50,
+                  color: selectedIndex == 6 ? Color(0xFFFF8828) : Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    selectedIndex = 6;
+                  });
+              
+                  widget.onIconTapped(6);
+                },
+              ),
             ),
           ),
         ],
